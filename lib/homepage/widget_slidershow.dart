daa80f6dc9b48508a4ed6b2dc0d2f707403cd3af
nitiphon1996 <ni.tiphon202@gmail.com>
2022-08-22 01:56:01 +0700

    update

--- lib/homepage/widget_slidershow.dart
+++ lib/homepage/widget_slidershow.dart
@@ -1,8 +1,10 @@
 // ignore_for_file: must_call_super
 
 import 'package:bizfull/boostrap/boostrap_tool.dart';
+import 'package:bizfull/tools/hex_color.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:get_storage/get_storage.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
 import 'package:youtube_player_iframe/youtube_player_iframe.dart';
 
@@ -14,6 +16,7 @@ class SliderTop extends StatefulWidget {
 }
 
 class _SliderState extends State<SliderTop> {
+  final box = GetStorage();
   List<int> list = [1, 2, 3, 4, 5];
   final List<String> imgList = [
     "images/slider.jpg",
@@ -25,6 +28,17 @@ class _SliderState extends State<SliderTop> {
     "images/sl6.jpg",
     "images/sl7.jpg",
   ];
+  final List<Color> clList = [
+    const Color(0xfff298b4),
+    const Color(0xffee4e2e),
+    const Color(0xff0593f9),
+    const Color(0xff08090b),
+    const Color(0xff131313),
+    const Color(0xffffd2d9),
+    const Color(0xff608bcf),
+    const Color(0xff20baee),
+  ];
+
   int _current = 0;
   final CarouselController _controller = CarouselController();
   bool autoPlaySet = false;
@@ -46,30 +60,69 @@ class _SliderState extends State<SliderTop> {
     };
   }
 
+  void setNewColor(index) {
+    // box.write("colorNav", clList[index]);
+    if (index == 0) {
+      box.write("colorNav", "#f298b4");
+    } else if (index == 1) {
+      box.write("colorNav", "#ee4e2e");
+    } else if (index == 2) {
+      box.write("colorNav", "#0593f9");
+    } else if (index == 3) {
+      box.write("colorNav", "#08090b");
+    } else if (index == 4) {
+      box.write("colorNav", "#131313");
+    } else if (index == 5) {
+      box.write("colorNav", "#ffd2d9");
+    } else if (index == 6) {
+      box.write("colorNav", "#608bcf");
+    } else if (index == 7) {
+      box.write("colorNav", "#608bcf");
+    } else if (index == 8) {
+      box.write("colorNav", "#20baee");
+    }
+
+    setState(() {
+      _current = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     EdgeInsets rdShow;
     double bSl;
     double sSl;
     double hSl;
+    double pdSl = 5;
+    String typeDv = "pc";
+    double ratosl = 5 / 1.6;
     if (Device.width > 991) {
       rdShow = const EdgeInsets.only(left: 5, bottom: 0, right: 5, top: 5.92);
       bSl = 15;
       sSl = 12;
       hSl = 4;
+      pdSl = 5;
+      typeDv = "pc";
+      ratosl = 5 / 1.6;
     } else if (Device.width >= 768 && Device.width <= 991) {
       rdShow = const EdgeInsets.only(left: 5, bottom: 0, right: 5, top: 0);
       bSl = 15;
       sSl = 10;
       hSl = 4;
+      pdSl = 0;
+      typeDv = "mobile";
+      ratosl = 2.47;
     } else {
       rdShow = const EdgeInsets.only(left: 5, bottom: 0, right: 5, top: 0);
       bSl = 11;
       sSl = 8;
       hSl = 3;
+      pdSl = 0;
+      typeDv = "mobile";
+      ratosl = 2.11;
     }
     return Padding(
-      padding: const EdgeInsets.only(left: 5, right: 5),
+      padding: EdgeInsets.only(left: pdSl, right: pdSl),
       child: BootstrapRow(
         children: <BootstrapCol>[
           BootstrapCol(
@@ -78,29 +131,39 @@ class _SliderState extends State<SliderTop> {
                 children: [
                   CarouselSlider(
                       options: CarouselOptions(
-                          aspectRatio: 5 / 1.6,
+                          aspectRatio: ratosl,
                           autoPlay: true,
-                          enlargeCenterPage: true,
+                          // enlargeCenterPage: true,
                           viewportFraction: 1,
                           enlargeStrategy: CenterPageEnlargeStrategy.height,
                           onPageChanged: (index, reason) {
-                            setState(() {
-                              _current = index;
-                            });
+                            setNewColor(index);
                           }),
                       items: imgList
                           .map((item) => Padding(
-                                padding: const EdgeInsets.only(
-                                    bottom: 5, left: 5, right: 5),
-                                child: Center(
-                                  child: SizedBox(
-                                    width: double.infinity,
-                                    child: Image.asset(
-                                      item,
-                                      filterQuality: FilterQuality.high,
-                                      fit: BoxFit.contain,
+                                padding: EdgeInsets.only(
+                                    bottom: 5, left: pdSl, right: pdSl),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    typeDv == "mobile"
+                                        ? Container(
+                                            height: 66,
+                                            color:
+                                                clList[imgList.indexOf(item)],
+                                          )
+                                        : const SizedBox(
+                                            height: 0,
+                                          ),
+                                    SizedBox(
+                                      width: double.infinity,
+                                      child: Image.asset(
+                                        item,
+                                        filterQuality: FilterQuality.high,
+                                        fit: BoxFit.contain,
+                                      ),
                                     ),
-                                  ),
+                                  ],
                                 ),
                               ))
                           .toList()),
